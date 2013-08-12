6d8435591fa1d2086a992fa5c2eba31c3bb6e8ea
Barry Carlyon <barry@barrycarlyon.co.uk>
2013-08-12 22:03:00 +0100

    added a website management screenshot

--- wp-zombaio/wp_zombaio_wizard.php
+++ wp-zombaio/wp_zombaio_wizard.php
@@ -7,6 +7,7 @@ switch ($step) {
         $this->options->wizard = true;
         $this->saveoptions();
         echo '<div id="message" class="updated"><p>' . __('All Done, you are ready to go', 'wp-zombaio') . '</p></div>';
+        echo '<div id="message" class="error"><p>' . __('Make sure when you are ready to go, to set your <strong>Site Profile, Production mode to LIVE</strong>, otherwise you will not be able to take any money!', 'wp-zombaio') . '</p></div>';
         echo __('<p>You can now review the current options and change advanced options</p>', 'wp-zombaio');
         $do = false;
         break;
@@ -22,8 +23,9 @@ switch ($step) {
                 . '<p>Now the final step</p>'
                 . '<p>Update the <strong>Postback URL (ZScript)</strong> to the following:</p>', 'wp-zombaio');
             echo $this->_postbackurl();
-            echo sprintf(__('<p>Then Press Validate</p>'
-                . '<p>Zombaio will then Validate the Settings, and if everything is correct, should say Successful and save the URL</p>'
+            echo sprintf(__('<p>Then Press <strong>Validate</strong></p>'
+                . '<p>Zombaio will then Validate the Settings, and check it can communciate with your site.</p>'
+                . '<p>If everything is correct, it should say Successful and save the URL</p>'
                 . '<p>If not, please <a href="%s">Click Here</a> and we will restart the Wizard to confirm your settings</p>'
                 . '<p>If everything worked, just hit Submit below</p>', 'wp-zombaio'), admin_url('admin.php?page=wp_zombaio&do=wizard'));
 
@@ -40,8 +42,8 @@ switch ($step) {
                 $this->saveoptions();
 
                 echo __('<h3>Communications - Gateway Password</h3>'
-                    . '<p>Next we need to setup the Zombaio -&gt; Communications</p>'
-                    . '<p>In Website Management, select Settings</p>'
+                    . '<p>Next we need to setup the Zombaio Communications back to your site</p>'
+                    . '<p>Under Tools then Website Management, select Settings on the profile for your Site</p>'
                     . '<p>Copy and Enter the <strong>Zombaio GW Pass</strong> below</p>', 'wp-zombaio');
                 echo '<label for="gw_pass">' . __('Zombaio GW Pass:', 'wp-zombaio') . ' <input type="text" name="gw_pass" id="gw_pass" value="' . $this->options->gw_pass . '" /></label>';
                 $nextstep = 2;
@@ -50,13 +52,15 @@ switch ($step) {
         }
     case '0':
     default:
-        echo __('<h3>Introduction</h3>'
+        echo sprintf(__('<h3>Introduction</h3>'
             . '<p>This Wizard will Guide you thru the Zombaio Setup</p>'
             . '<h3>Site ID</h3>'
-            . '<p>First your will need a Zombaio Account</p>'
-            . '<p>And to have added your Website under Website Management</p>'
-            . '<p>This will give you a <strong>Site ID</strong>, enter that now:</p>', 'wp-zombaio');
+            . '<p>First your will need a <a href="%s" target="zombaio">Zombaio Account</a></p>'
+            . '<p>Then you can login to <a href="%s" target="zombaio">ZOA</a></p>'
+            . '<p>Under Tools then Website Management (<a href="#nowhere" onclick="%s">Screenshot</a>), you will need to create a profile for your Site, if you have not already</p>'
+            . '<p>This will give you a <strong>Site ID</strong>, enter that now:</p>', 'wp-zombaio'), 'https://www.zombaio.com/', 'https://secure.zombaio.com/zoa/', 'jQuery(\'#wp_zombaio_website_management\').dialog({width: 720});');
         echo '<label for="site_id">' . __('Site ID:', 'wp-zombaio') . ' <input type="text" name="site_id" id="site_id" value="' . $this->options->site_id . '" /></label>';
+        echo '<div id="wp_zombaio_website_management" style="display: none;"><img src="' . plugin_dir_url(__FILE__) . '/assets/screenshots/website_management.jpg" style="width: 700px;" alt="' . __('Website Management', 'wp-zombaio') . '" /></div>';
         $nextstep = 1;
 }
 echo '<input type="hidden" name="step" value="' . $nextstep . '" />';
